a4e005697c070f62b6fb6e174379548e47ecc332
Eduardo Sa <nofrereis@gmail.com>
2021-06-09 14:23:50 -0300

    primeiro commit

--- Micro-esqueleto/src/main/java/com/estudo/jpa/EstudoJpaApplication.java
+++ Micro-esqueleto/src/main/java/com/estudo/jpa/EstudoJpaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroEsqueletoApplication {
+public class EstudoJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroEsqueletoApplication.class, args);
+		SpringApplication.run(EstudoJpaApplication.class, args);
 	}
 
 }
