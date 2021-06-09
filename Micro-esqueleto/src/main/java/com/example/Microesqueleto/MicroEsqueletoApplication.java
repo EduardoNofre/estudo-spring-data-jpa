f0b6c562c438a4d38857ed03d1110d2e4d8ba9d4
Eduardo Sa <nofrereis@gmail.com>
2021-06-09 08:57:39 -0300

    primeiro commit

---
+++ Micro-esqueleto/src/main/java/com/example/Microesqueleto/MicroEsqueletoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microesqueleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEsqueletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEsqueletoApplication.class, args);
+	}
+
+}
---
+++ Micro-esqueleto/src/test/java/com/example/Microesqueleto/MicroEsqueletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microesqueleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEsqueletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
